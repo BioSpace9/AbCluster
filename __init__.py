9b37484a46fef2a93e89499ed984caa6b3ab96f7
BioSpace9 <78736963+BioSpace9@users.noreply.github.com>
2021-02-11 19:39:09 +0900

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@
 from .AbCluster import *
 
 __copyright__    = 'Copyright (C) 2020 BioSpace'
-__version__      = '1.0.0'
+__version__      = '1.1.0'
 __license__      = 'Apache License 2.0'
 __author__       = 'BioSpace'
 __author_email__ = 'biospaceinfo@gmail.com'
